cf6f7c19054d0e6b56e3be77c4eadaa8712624f6
chrinkus <seeschickrun@gmail.com>
2020-02-24 13:08:26 -0600

    Removed '2dos' functionality from marksout

--- marks_out/marks_out.sh
+++ marks_out/marks_out.sh
@@ -4,14 +4,11 @@
 #
 # prints to stdout by default.
 #
-# -o, --outfile
+# -f, --file
 #	prints to '.txt' file with name generated from input
 #
-# -u, --unix
-#	output has UNIX-style line-endings (default)
-#
-# -w, --windows
-#	output has DOS-style line-endings
+# -q, --quiet
+#	no diagnostic messages are printed
 
 extract_records() {
 	while read line; do
@@ -21,47 +18,32 @@ extract_records() {
 	done
 }
 
-format_line_endings() {
-	if [[ $1 -gt 0 ]]; then
-		awk 'sub("$", "\r")'
-	else
-		awk '{ print }'
-	fi
-}
-
 extract_out_filename() {
 	echo "$1" |
-		sed -E 's/^([A-Z]{4}-[0-9]{4})..([0-9]{6}).*$/\1_\2.txt/'
+		sed -E 's/^([A-Z]{4}-[0-9]{4}).*([0-9]{6}).*$/\1_\2.txt/'
 }
 
-WIN=0
-OUT=0
+logfile=/dev/stdout
+verbose=1
 
 while [[ $# -gt 1 ]]; do
 	case "$1" in
-		-w|--windows)
-			WIN=1
-			echo "Exporting with Windows line-endings"
+		-f|--file)
+			logfile=$(extract_out_filename "$2")
 			;;
-		-u|--unix)
-			WIN=0
-			echo "Exporting with Unix line-endings"
-			;;
-		-o|--outfile)
-			OUT=1
+		-q|--quiet)
+			verbose=0
 			;;
 	esac
 	shift
 done
 
-DEST="&1"
-if [ $OUT ]; then
-	DEST="$(extract_out_filename "$1")"
+if [[ $verbose -eq 1 ]]; then
+	echo "Outputting to ${logfile}.."
 fi
 
 cat "$1" |
 	extract_records |
 	sed -E 's/#(.*)/\1/' |
 	awk -F, '{ printf "%s\t%-20s%-15s%.0f\n", $1, $2, $3, $4 }' |
-	sort -k 2,3 |
-	format_line_endings "$WIN" > "$DEST"
+	sort -k 2,3
